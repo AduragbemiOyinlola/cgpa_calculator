04dba7397e5fcac8d2ef621c59ee538aa2465665
Aduragbemi <aduragbemioyinlola@gmail.com>
2025-01-08 14:31:51 +0100

    get_year method

--- oop class.py
+++ oop class.py
@@ -1,2 +1,11 @@
 class CGPA:
+    def __init__(self):
+        self.grades = []
+        self.units = []
+        self.w_s = 0
+        self.grade_points = {'A':5, 'B':4, 'C':3, 'D':2, 'E':1, 'F':0}
+
+    def get_year(self, year):
+        self.year = int(input(year))
+
     
